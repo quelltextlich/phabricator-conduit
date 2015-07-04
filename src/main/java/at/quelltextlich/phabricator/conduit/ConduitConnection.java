3043180bc521bbd47450ce765673be62791fd8b7
Christian Aistleitner <christian@quelltextlich.at>
2015-07-05 00:32:24 +0200

    Remove unused imports
    Change-Id: I2e5808a8d8943da16c4e20a8cdd13f10af08f73d

--- src/main/java/at/quelltextlich/phabricator/conduit/ConduitConnection.java
+++ src/main/java/at/quelltextlich/phabricator/conduit/ConduitConnection.java
@@ -30,7 +30,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
